a3fdd7f93150cd1b4b06c64aa9ec4cf320065b08
pfsjdsn <canoecanoe@aliyun.com>
2023-01-04 10:05:38 +0800

    原始数据类型和 Any 类型

---
+++ typescript-base/base-types.ts
@@ -0,0 +1,18 @@
+//  原始数据类型和 Any 类型
+
+let isDone:boolean  = false
+ 
+let age:number  = 10
+
+let firstName:string = '123456'
+
+let message:string = `hello, ${firstName}`
+
+let u:undefined = undefined
+
+let n:null = null
+
+let notSure:any  = 4
+notSure = '111'
+notSure.myName
+notSure.getName()
