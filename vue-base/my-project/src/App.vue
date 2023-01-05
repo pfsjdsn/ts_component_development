7f4ac950cae6e2d25a48ace68e5b7f9bfa906c56
pfsjdsn <canoecanoe@aliyun.com>
2023-01-05 16:28:18 +0800

    响应式对象(可修改对象和数组)

--- vue-base/my-project/src/App.vue
+++ vue-base/my-project/src/App.vue
@@ -2,6 +2,10 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>{{count}}</h1>
   <h1>{{double}}</h1>
+  <ul>
+    <li v-for="number in numbers" :key="number"><h1>{{ number }}</h1></li>
+  </ul>
+  <h1>{{person.name}}</h1>
   <button @click="increase">+1</button>
 </template>
 
@@ -11,6 +15,8 @@ interface DataProps{
   count:number;
   double:number;
   increase:()=>void;
+  numbers:number[];
+  person:{name?:string}
 }
 export default {
   name: 'App', 
@@ -38,8 +44,12 @@ export default {
       const data:DataProps = reactive({
         count:0,
         increase:()=>{data.count++},
-        double:computed(() =>data.count *2)
+        double:computed(() =>data.count *2),
+        numbers:[0,1,2],
+        person:{}
       })
+      data.numbers[0] =5
+      data.person.name="xiaoming"
       const refData =toRefs(data)
       return{
         ...refData
