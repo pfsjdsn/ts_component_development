97ffd2dcca5b0ede03e0346b4328a45e30a76556
pfsjdsn <canoecanoe@aliyun.com>
2023-01-05 16:00:12 +0800

    vue3 ref

---
+++ vue-base/my-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>{{count}}</h1>
+  <h1>{{double}}</h1>
+  <button @click="increase">+1</button>
+</template>
+
+<script lang="ts">
+import {ref,computed} from 'vue'
+export default {
+  name: 'App', 
+  setup() {
+      const count = ref(0)
+      const double = computed(()=>{
+        return count.value *2
+      })
+      const increase = () =>{
+        count.value++
+      }
+      return{
+        count,
+        increase,
+        double
+      }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
