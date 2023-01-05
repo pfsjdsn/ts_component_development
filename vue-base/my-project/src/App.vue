85066175faba327431764c424ef5052d284f340c
pfsjdsn <canoecanoe@aliyun.com>
2023-01-05 16:17:02 +0800

    vue3 reactive

--- vue-base/my-project/src/App.vue
+++ vue-base/my-project/src/App.vue
@@ -6,21 +6,43 @@
 </template>
 
 <script lang="ts">
-import {ref,computed} from 'vue'
+import {ref,computed,reactive,toRefs} from 'vue'
+interface DataProps{
+  count:number;
+  double:number;
+  increase:()=>void;
+}
 export default {
   name: 'App', 
   setup() {
-      const count = ref(0)
-      const double = computed(()=>{
-        return count.value *2
+    /**
+     * ref的使用
+     */
+      // const count = ref(0)
+      // const double = computed(()=>{
+      //   return count.value *2
+      // })
+      // const increase = () =>{
+      //   count.value++
+      // }
+      // return{
+      //   count,
+      //   increase,
+      //   double
+      // }
+
+      /**
+       * reactive
+       * toRefs
+       */
+      const data:DataProps = reactive({
+        count:0,
+        increase:()=>{data.count++},
+        double:computed(() =>data.count *2)
       })
-      const increase = () =>{
-        count.value++
-      }
+      const refData =toRefs(data)
       return{
-        count,
-        increase,
-        double
+        ...refData
       }
   },
 }
