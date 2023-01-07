467554b960ed9a8b243ba61e9647ac67aa070f23
pfsjdsn <canoecanoe@aliyun.com>
2023-01-07 15:23:25 +0800

    Dropdown 第三部分(点击除了下拉框位置，也可以关闭当前下拉框)

--- zhihu-column/src/components/Dropdown.vue
+++ zhihu-column/src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dropdown">
+    <div class="dropdown" ref="dropdownRef">
         <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{ title }}</a>
         <ul class="dropdown-menu" :style="{display:'block'}" v-if="isOpen">
             <li><a class="dropdown-item" href="#">新建文章</a></li>
@@ -9,7 +9,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from "vue";
+import { defineComponent, PropType, ref, onMounted, onUnmounted } from "vue";
 export default defineComponent({
     name: "DropDown",
     props: {
@@ -20,12 +20,28 @@ export default defineComponent({
     },
     setup() {
         const isOpen = ref(false)
+        const dropdownRef = ref<null | HTMLElement>(null)
         const toggleOpen = () => {
             isOpen.value = !isOpen.value
         }
+        const handler = (e: MouseEvent) => {
+            // 点击除了下拉框位置，也可以关闭当前下拉框
+            if (dropdownRef.value) {
+                if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+                    isOpen.value = false
+                }
+            }
+        }
+        onMounted(() => {
+            document.addEventListener('click', handler)
+        })
+        onUnmounted(() => {
+            document.removeEventListener('click', handler)
+        })
         return {
             isOpen,
-            toggleOpen
+            toggleOpen,
+            dropdownRef
         }
     }
 
