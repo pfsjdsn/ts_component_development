d2e04fdc30caaad7c041bb3ea7a339f191d4574e
pfsjdsn <canoecanoe@aliyun.com>
2023-01-07 11:58:14 +0800

    ColumnList 编码第二部分

--- zhihu-column/src/App.vue
+++ zhihu-column/src/App.vue
@@ -13,13 +13,25 @@ const testData: ColumnProps[] = [
     id: 1,
     title: "test1的专栏",
     description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar: "https://images.dog.ceo/breeds/whippet/n02091134_18069.jpg",
+    avatar: require("./assets/logo.png"),
   },
   {
     id: 2,
     title: "test1的专栏",
     description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar: "https://images.dog.ceo/breeds/mountain-bernese/n02107683_4016.jpg",
+    // avatar: require("./assets/logo.png"),
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这是的test3专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar: require("./assets/logo.png"),
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是的test4专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar: require("./assets/logo.png"),
   },
 ];
 export default defineComponent({
