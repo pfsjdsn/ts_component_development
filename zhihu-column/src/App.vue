7869f8917d0750f3a5ba43ffea650178524f603c
pfsjdsn <canoecanoe@aliyun.com>
2023-01-07 14:21:38 +0800

    GobalHeader 组件

--- zhihu-column/src/App.vue
+++ zhihu-column/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="testData"></column-list>
   </div>
 </template>
@@ -8,6 +9,11 @@
 import { defineComponent } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
+const currentUser: UserProps = {
+  isLogin: true,
+  name: "admin",
+};
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -38,10 +44,12 @@ export default defineComponent({
   name: "App",
   components: {
     ColumnList,
+    GlobalHeader,
   },
   setup() {
     return {
       testData,
+      currentUser,
     };
   },
 });
