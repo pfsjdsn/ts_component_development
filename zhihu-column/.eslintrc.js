c7e6c52a7c68d1ee3cb6839b4a4a8359de711f5f
pfsjdsn <canoecanoe@aliyun.com>
2023-01-07 10:59:21 +0800

    ColumnList 编码第一部分

--- zhihu-column/.eslintrc.js
+++ zhihu-column/.eslintrc.js
@@ -12,6 +12,11 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    "quotes": 'off',
+    "semi": 'off',
+    "comma-dangle": 'off',
+    'semi': [0],
+    "space-before-function-paren": 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- zhihu-column/src/App.vue
+++ zhihu-column/src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="testData"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1的专栏",
+    description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar: "https://images.dog.ceo/breeds/whippet/n02091134_18069.jpg",
+  },
+  {
+    id: 2,
+    title: "test1的专栏",
+    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar: "https://images.dog.ceo/breeds/mountain-bernese/n02107683_4016.jpg",
+  },
+];
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    ColumnList,
+  },
+  setup() {
+    return {
+      testData,
+    };
+  },
+});
 </script>
 
 <style>
